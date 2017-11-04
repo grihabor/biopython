f70d63777076929764cccc39ffc3bcf79413e2e3
Borodin Gregory <grihabor@mail.ru>
2017-11-05 01:18:08 +0300

    generate rst files

--- doc/utils/convert.py
+++ doc/utils/convert.py
@@ -1,21 +1,40 @@
+import subprocess
 import os
 import sys
+import itertools
 
 
-def iterate_rst(doc_root):
+def iterate_tex(doc_root):
     for root, dirnames, filenames in os.walk(doc_root):
         for filename in filenames:
-            if filename.endswith('.rst'):
-                path = os.path.join(root, filename)
-                yield path
+            if filename.endswith('.tex'):
+                tex_path = os.path.join(root, filename)
+                rst_path = tex_path[:-4] + '.rst'
+                yield tex_path, rst_path
 
 
-def apply_pandoc(doc_root):
-    for path in iterate_rst()
+def generate_rst_from_tex(tex_path, rst_path):
+    with open(rst_path, 'w') as rst:
+        print('Writing to {}'.format(rst_path))
+        return subprocess.run([
+            'pandoc',
+            '--from', 'latex',
+            '--to', 'rst',
+            tex_path,
+        ], stdout=rst)
+
+
+def generate_rst(doc_root):
+    for result in itertools.starmap(
+        generate_rst_from_tex,
+        iterate_tex(doc_root)
+    ):
+        print(result)
 
 
 def main():
-    doc_root = sys.argv[1]
+    doc_root = os.path.abspath(sys.argv[1])
+    generate_rst(doc_root)
 
 
 if __name__ == '__main__':
